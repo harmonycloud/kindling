b55f3374d6c7337f572a2ebf572954bcd1ca51c1
J-lena <yufangjiang@harmonycloud.cn>
2026-07-20 15:48:01 +0800

    update release info (#661)
    Signed-off-by: jiangyufang <jiangyufang@harmonycloud.cn>
    Co-authored-by: jiangyufang <jiangyufang@harmonycloud.cn>

--- deploy/recompile-probe/recompile-module.sh
+++ deploy/recompile-probe/recompile-module.sh
@@ -1,15 +1,13 @@
-AGENT_LIBS_COMMIT_ID=5a0edd80166ea059336945a3771eb565885adf26
-
 mkdir -p rebuild-kindling-agent
 cd rebuild-kindling-agent
-curl -O https://k8s-bpf-probes-public.oss-cn-hangzhou.aliyuncs.com/agent-libs-${AGENT_LIBS_COMMIT_ID}.tar.gz
-tar -zvxf agent-libs-${AGENT_LIBS_COMMIT_ID}.tar.gz
-cd agent-libs-${AGENT_LIBS_COMMIT_ID}
+curl -O https://github.com/harmonycloud/agent-libs/archive/refs/tags/kindling-falcolib-probe-29721003629.tar.gz
+tar -zvxf kindling-falcolib-probe-29721003629.tar.gz -C agent-libs
+cd agent-libs
 docker pull kindlingproject/kernel-builder:latest
 docker run -it -v /usr:/host/usr -v /lib/modules:/host/lib/modules -v $PWD:/source kindlingproject/kernel-builder:latest
 cd ..
 cat <<EOF > Dockerfile
 FROM kindlingproject/kindling-agent:latest
-COPY ./agent-libs-${AGENT_LIBS_COMMIT_ID}/kindling-falcolib-probe/* /opt/kindling-extra-probe/
+COPY ./agent-libs/kindling-falcolib-probe/* /opt/kindling-extra-probe/
 EOF
 docker build -t kindlingproject/kindling-agent:latest-bymyself .
